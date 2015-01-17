8dce94f75233021376125ceeec064d5b904e89be
Obipolar <shaohuixu13@gmail.com>
2015-01-16 23:44:37 -0500

    Repo Init with Yeoman Generation

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name motionCaptchaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the motionCaptchaApp
+ */
+angular.module('motionCaptchaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name motionCaptchaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the motionCaptchaApp
+ */
+angular.module('motionCaptchaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
